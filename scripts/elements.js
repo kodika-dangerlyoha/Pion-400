88c939ee5d3ca12f2ec5c05c3fc9b078974dc528
ladn0 <84666429+kodika-dangerlyoha@users.noreply.github.com>
2025-06-18 23:25:25 +0300

    +Left navigation

---
+++ scripts/elements.js
@@ -0,0 +1,3 @@
+const get_html_ = () => {
+    
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,4 @@
-script.js
+const show_tournament = (title) => {
+    document.querySelector('.navigation__games__game_active').classList.remove('navigation__games__game_active');
+    document.querySelector(`#nav_games_${title}`).classList.add('navigation__games__game_active');
+}

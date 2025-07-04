48495dfb744201207c91a62807d945039f372995
ladn0 <84666429+kodika-dangerlyoha@users.noreply.github.com>
2025-07-04 15:38:18 +0300

    04.07.25-1
    + Right Aside
    + Ховер для ссылок в шапке

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,19 @@ const show_tournament = (title) => {
     document.querySelector('.navigation__games__game_active').classList.remove('navigation__games__game_active');
     document.querySelector(`#nav_games_${title}`).classList.add('navigation__games__game_active');
 }
+
+const toggle_bettingHistory = () => {
+    document.querySelector('.bettingHistory').classList.toggle('bettingHistory_hidden');
+    const bettingLog_active = document.querySelector('.bettingLog_active');
+    if (bettingLog_active) {
+        bettingLog_active.classList.remove('bettingLog_active');
+    }
+}
+
+const show_pari = () => {
+    document.querySelector('.bettingLog').classList.toggle('bettingLog_active');
+    
+    if (!document.querySelector('.bettingHistory_hidden')) {
+        document.querySelector('.bettingHistory').classList.add('bettingHistory_hidden');
+    }
+}
